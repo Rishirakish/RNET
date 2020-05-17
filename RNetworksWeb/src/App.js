71ffd7644d5e81b5dd46104e610ec5a8096fcbe9
Singh, Manish <Manish.Singh3@emc.com>
2020-05-17 14:25:32 +0530

    Added test plan form page
    Added test plan form page

--- RNetworksWeb/src/App.js
+++ RNetworksWeb/src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 import SignIn from "./components/signin";
+import TestPlan from "./components/testplan"
 //const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <SignIn ></SignIn>
+      <TestPlan ></TestPlan>
     );
   }
 }
--- RNetworksWeb/src/sagas/counter.js
+++ RNetworksWeb/src/sagas/counter.js
@@ -1,9 +1,9 @@
 import { UPDATE_COUNTER, UPDATE_COMPLETE } from "../actions/counterActions";
-import { delay } from "redux-saga";
+// import { delay } from "redux-saga";
 import { put, takeEvery } from "redux-saga/effects";
 
 export function* count(action) {
-  yield delay(1000); //Delay the Counter By 1 Second
+  // yield delay(1000); //Delay the Counter By 1 Second
   yield put({ type: UPDATE_COMPLETE, payload: action.payload }); //Send the Final Payload to the Reducer
 }
 
