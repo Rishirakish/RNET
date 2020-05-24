10bb8126dab253c0d929a52243edf2370bf9a670
Singh, Manish <Manish.Singh3@emc.com>
2020-05-24 09:28:36 +0530

    updated routes
    updated routes

--- RNetworksWeb/src/App.js
+++ RNetworksWeb/src/App.js
@@ -4,14 +4,23 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 import SignIn from "./components/signin";
 import TestPlan from "./components/testplan"
-//const store = configureStore();
-
-class App extends Component {
-  render() {
-    return (
-      <TestPlan ></TestPlan>
-    );
-  }
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+function App() {
+  const global = React.createContext('globals');
+  return (<global.Provider value="123"><Router>
+    
+    <div className="App">
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SignIn} />
+            <Route path="/testplan" component={TestPlan} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router></global.Provider>
+    
+  );
 }
 
 export default App;
