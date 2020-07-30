5f0b60f4048a92f8e6f94b8e208cdd5f0e5ffbcc
Byresh P <byresh.papaiah@rsa.com>
2020-07-30 20:13:20 +0530

    Cleaning up Identity project

--- TestingAndCalibrationLabs/TestingAndCalibrationLabs.Identity.Core/Entity/SampleEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestingAndCalibrationLabs.Identity.Core.Entity
-{
-    public class SampleEntity : SharedKernel.Base.Entity.Entity
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
